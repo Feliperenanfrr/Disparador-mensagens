6181d98ad6806c0acef1f4a21840dade4328e8df
Felipe Renan <felipeferreira3146@gmail.com>
2024-04-27 10:44:39 -0300

    Adicionando as funções de buscar contato por ID e verificar validação

--- WinFormsApp2/Util/ConexaoAPI.cs
+++ WinFormsApp2/Util/ConexaoAPI.cs
@@ -11,7 +11,7 @@ namespace Gweb.WhatsApp.Util
 {
     internal class ConexaoAPI
     {
-        public string obterToken(string email, string senha)
+        public string ObterToken(string email, string senha)
         {
             var client = new RestClient("https://api.underchat.com.br/");
             var request = new RestRequest("/user/auth", Method.POST);
@@ -43,7 +43,7 @@ namespace Gweb.WhatsApp.Util
             }
         }
 
-        public dynamic buscarContatoPorNumero(string idLoja, string numero, string token, int tentativas = 0)
+        public dynamic BuscarContatoPorNumero(string idLoja, string numero, string token, int tentativas = 0)
         {
             var client = new RestClient("https://api.underchat.com.br/");
             var request = new RestRequest($"/store/{idLoja}/contact", Method.GET);
@@ -71,17 +71,58 @@ namespace Gweb.WhatsApp.Util
                 {
                     var semNoveDigitos = phoneNumberUtil.Format(numeroBrasil, PhoneNumberFormat.RFC3966);
                     semNoveDigitos = semNoveDigitos.Replace("tel:", "");
-                    return buscarContatoPorNumero(idLoja, semNoveDigitos, token, 1);
+                    return BuscarContatoPorNumero(idLoja, semNoveDigitos, token, 1);
                 }
                 else
                 {
                     var comNoveDigitos = phoneNumberUtil.Format(numeroBrasil, PhoneNumberFormat.RFC3966);
                     comNoveDigitos = comNoveDigitos.Replace("tel:", "");
-                    return buscarContatoPorNumero(idLoja, comNoveDigitos, token, 1);
+                    return BuscarContatoPorNumero(idLoja, comNoveDigitos, token, 1);
                 }
             }
 
             return null;
+        }
+
+        public static dynamic BuscarContatoPorId(string idLoja, string id, string token)
+        {
+            var client = new RestClient("https://api.example.com");
+            var request = new RestRequest($"/store/{idLoja}/contact", Method.GET);
+            request.AddParameter("filter[id]", id);
+            request.AddParameter("fields", "id,status,value");
+            request.AddHeader("Authorization", "Bearer " + token);
+
+            var response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(response.Content);
+                var contato = data[0];
+                return contato;
+            }
+
+            return null;
+        }
+
+        public static void VerificaSeContatoFoiValidado(string idLoja, string idContato, string token, int tentativas = 1)
+        {
+            //Console.WriteLine($"[{DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss")}] Verificando se o contato já foi validado ({tentativas}º Tentativa)...");
+
+            dynamic contato = BuscarContatoPorId(idLoja, idContato, token);
+            bool isValidated = contato != null && contato.status == "VALIDATED";
+
+            if (!isValidated)
+            {
+                if (tentativas < 10)
+                {
+                    Thread.Sleep(1000); // Aguarda 1 segundo antes de tentar novamente
+                    VerificaSeContatoFoiValidado(idLoja, idContato, token, tentativas + 1);
+                }
+                else
+                {
+                    throw new Exception("O contato não foi validado, tente novamente mais tarde.");
+                }
+            }
         }
 
 
